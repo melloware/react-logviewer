3c1120f2069aad502965bf02b749a2de3c7310ed
Melloware <mellowaredev@gmail.com>
2024-11-10 08:06:06 -0500

    Storybook 8.4.2 (#60)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,19 @@
-module.exports = {
-    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-scss",
-    ],
-    features: {
-        previewMdx2: true,
-    },
-    framework: {
-        name: "@storybook/react-webpack5",
-        options: {},
-    },
-    docs: {
-        autodocs: true,
-    }
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {}
 };
+export default config;
---
+++ docs/assets/preview-C8YAHaTL.js
@@ -0,0 +1,1 @@
+const o={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},tags:["autodocs"]};export{o as default};
