2629d9ad501e3a384cdfbe3ee1555fa3a7454bdd
Jesse Klotz <jesseklotz2306@gmail.com>
2024-11-22 00:56:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	rollupOptions: {
+	  // make sure to externalize deps that shouldn't be bundled
+	  // into your library
+	  external: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/client'],
+	  output: {
+		exports: "named",
+		// Provide global variables to use in the UMD build
+		// for externalized deps
+		globals: {
+		  react: "React",
+		  "react-dom": "ReactDOM",
+		},
+	  },
 })
+
+
